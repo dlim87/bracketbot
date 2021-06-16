8a7d48fb7968e211b85f28b20d728633489305b6
Daniel Lim <dlim87@gmail.com>
2021-06-16 10:44:31 -0700

    render a button

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const dotenv = require('dotenv')
 dotenv.config()
+const disbut = require('discord-buttons')
+disbut(client)
 
 client.once('ready', () =>{
     console.log("Bot Ready!")
@@ -9,6 +11,14 @@ client.once('ready', () =>{
 
 client.login(process.env.TOKEN)
 
+let button = new disbut.MessageButton()
+    .setLabel('one')
+    .setID('one')
+    .setStyle('blurple')
+
 client.on('message', message => {
     console.log(`message recieved: ${message.content}`)
+    if(message.content=='!buttons') {
+        message.channel.send("you wanted some buttons?", button)
+    }
 })
